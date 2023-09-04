ba297fdb545bd173c17113eb165f1a6672ce8910
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-09-04 22:43:34 +0000

    EDITASRTGA

--- src/models/to-do/model.js
+++ src/models/to-do/model.js
@@ -2,7 +2,7 @@
 
 const TodoModel = (sequelize, DataTypes) => sequelize.define('Todo', {
   item: { type: DataTypes.STRING, required: true },
-  assignedTo: { type: DataTypes.INTEGER, required: true },
+  assignedTo: { type: DataTypes.STRING, required: true },
   difficulty: { type: DataTypes.INTEGER, required: true },
   complete:{ type: DataTypes.BOOLEAN, required: true }
 });
