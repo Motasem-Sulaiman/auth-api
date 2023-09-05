9399f55d9e6301dc2e253ad1645cd8ecadbbeb89
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-09-05 05:46:01 +0000

    edit cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const errorHandler = require("./error-handlers/500.js");
 const logger = require("./middleware/logger.js");
 const v1Routes = require("./routes/v1.js");
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173,https://vvlvtj-5173.csb.app/');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); // Allow the Content-Type header
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
   // Add other HTTP methods if needed
